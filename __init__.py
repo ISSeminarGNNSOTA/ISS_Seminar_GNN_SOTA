511b201640f62fee692f65d3cd327dc63b69e929
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-08 18:17:21 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# Ali Jaabous
-# __init__- file for the models 
-
-from models.graph_convolutional_network import GCN
-from models.graph_sage_network import GraphSAGE
-from models.graph_attentional_network import GAT
