39f4beeb66cc3f0a78fe658445f54c1e1bd263b9
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-11 13:45:12 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@
 from models.graph_convolutional_network import GCN
 from models.graph_sage_network import GraphSAGE
 from models.graph_attentional_network import GAT
+from models.graph_predictor import Predictor
