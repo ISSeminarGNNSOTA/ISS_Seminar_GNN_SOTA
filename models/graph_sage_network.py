86f3b34367c28eade08bf3632b704815b2fdbf05
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-11 12:50:08 +0100

    Update graph_sage_network.py

--- models/graph_sage_network.py
+++ models/graph_sage_network.py
@@ -8,18 +8,17 @@ class Encoder(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, dropout_rate=0.5):
         super().__init__()
         self.conv1 = SAGEConv(-1, hidden_channels)
-        self.dropout1 = nn.Dropout(p=dropout_rate)
         self.conv2 = SAGEConv(hidden_channels*1, hidden_channels)
-        self.dropout2 = nn.Dropout(p=dropout_rate)
         self.conv3 = SAGEConv(hidden_channels*1, out_channels)
+        self.dropout = nn.Dropout(p=dropout_rate)
 
     def forward(self, x, edge_index, edge_weight):
         x = self.conv1(x, edge_index, edge_weight)
         x = F.relu(x)
-        x = self.dropout1(x)
+        x = self.dropout(x)
         x = self.conv2(x, edge_index, edge_weight)
         x = F.relu(x)
-        x = self.dropout2(x)
+        x = self.dropout(x)
         x = self.conv3(x, edge_index, edge_weight)
         return x
 
@@ -27,18 +26,17 @@ class Predictor(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, out_dim, dropout_rate=0.5):
         super().__init__()
         self.fc1 = nn.Linear(input_dim*2, hidden_dim)
-        self.dropout1 = nn.Dropout(p=dropout_rate)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.dropout2 = nn.Dropout(p=dropout_rate)
         self.fc3 = nn.Linear(hidden_dim, out_dim)
+        self.dropout = nn.Dropout(p=dropout_rate)
 
     def forward(self, x, edge_label_index):
         row, col = edge_label_index
         x = torch.cat([x['user'][row], x['movie'][col]], dim=-1)
         x = F.relu(self.fc1(x))
-        x = self.dropout1(x)
+        x = self.dropout(x)
         x = F.relu(self.fc2(x))
-        x = self.dropout2(x)
+        x = self.dropout(x)
         x = self.fc3(x)
         return x
 
