eef20a63dee52e697a55009517c0cc9f6bd3a995
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-11 13:27:01 +0100

    Update graph_convolutional_network.py

--- models/graph_convolutional_network.py
+++ models/graph_convolutional_network.py
@@ -7,6 +7,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import Graph_predictor as Predictor
+
 class Encoder(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, dropout_rate=0.5):
         super().__init__()
@@ -23,24 +25,6 @@ class Encoder(torch.nn.Module):
         x = F.relu(x)
         x = self.dropout(x)
         x = self.conv3(x, edge_index, edge_weight)
-        return x
-
-class Predictor(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, out_dim, dropout_rate=0.5):
-        super().__init__()
-        self.fc1 = nn.Linear(input_dim*2, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, out_dim)
-        self.dropout = nn.Dropout(p=dropout_rate)
-
-    def forward(self, x, edge_label_index):
-        row, col = edge_label_index
-        x = torch.cat([x['user'][row], x['movie'][col]], dim=-1)
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = self.fc3(x)
         return x
 
 class GCN(torch.nn.Module):
