863628cbc3f5d6e1456ac393e746582122758f50
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-12 13:45:53 +0100

    Update __init__.py

--- data_preprocessing/__init__.py
+++ data_preprocessing/__init__.py
@@ -1,7 +1,9 @@
 # Ali Jaabous
-# __init__- file for the models 
+# __init__- file for data_preprocessing 
 
 from data_preprocessing.DataPreprocessor import filter_data
 from data_preprocessing.DataPreprocessor import create_rating_user_movie_info
 from data_preprocessing.DataPreprocessor import perform_nlp_tasks
 from data_preprocessing.DataPreprocessor import get_node_embeddings
+
+from data_preprocessing.MovieTitlesProcessor import process_movie_title
