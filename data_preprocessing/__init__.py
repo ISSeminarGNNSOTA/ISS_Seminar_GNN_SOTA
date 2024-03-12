cac896beb153056857ac21d095be70a59cbbe60e
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-12 13:42:55 +0100

    Update and rename __init__ (1).py to __init__.py

---
+++ data_preprocessing/__init__.py
@@ -0,0 +1,7 @@
+# Ali Jaabous
+# __init__- file for the models 
+
+from data_preprocessing.DataPreprocessor import filter_data
+from data_preprocessing.DataPreprocessor import create_rating_user_movie_info
+from data_preprocessing.DataPreprocessor import perform_nlp_tasks
+from data_preprocessing.DataPreprocessor import get_node_embeddings
