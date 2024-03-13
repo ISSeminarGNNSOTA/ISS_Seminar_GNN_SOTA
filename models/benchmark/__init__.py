ebd4e069dc52ee966f5ddc7c65c40404f1c1b8ee
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-13 15:48:49 +0100

    Update __init__.py

--- models/benchmark/__init__.py
+++ models/benchmark/__init__.py
@@ -1,1 +1,7 @@
+#from xgb import XGBModel
+#from lr_ridge import LRModel
+#from mlp import MLPModelOptimized
 
+from models.xgb import XGBModel
+from models.lr_ridge import LRModel
+from models.mlp import MLPModelOptimized
