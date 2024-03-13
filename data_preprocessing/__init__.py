990266af7638abec6b62102c4faa404db1b1590b
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-13 22:20:30 +0100

    Update __init__.py

--- data_preprocessing/__init__.py
+++ data_preprocessing/__init__.py
@@ -1,11 +1,11 @@
-# Ali Jaabous
-# __init__- file for data_preprocessing 
 
-#from data_preprocessing.data_preprocess_v4 import filter_data
-#from data_preprocessing.data_preprocess_v4 import create_rating_user_movie_info
-#from data_preprocessing.data_preprocess_v4 import perform_nlp_tasks
-#from data_preprocessing.data_preprocess_v4 import get_node_embeddings
 
-from data_preprocessing.data_preprocess_v4 import DataPreprocessor # thoose two could be enough to make the notebook work
 
+from data_preprocessing.data_preprocess_v5 import DataPreprocessor # thoose two could be enough to make the notebook work
 from data_preprocessing.movtit_nlp_v3 import MovieTitlesProcessor # thoose two could be enough to make the notebook work
+
+#from data_preprocessing.data_preprocess_v5 import filter_data
+#from data_preprocessing.data_preprocess_v5 import create_rating_user_movie_info
+#from data_preprocessing.data_preprocess_v5 import perform_nlp_tasks
+#from data_preprocessing.data_preprocess_v5 import get_node_embeddings
+
