cc001c92077dcab36f05e0baee20e8fcefd948a8
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-14 01:22:36 +0100

    Update __init__.py

--- data_preprocessing/__init__.py
+++ data_preprocessing/__init__.py
@@ -1,8 +1,5 @@
 from data_preprocessing.data_preprocess_v5 import DataPreprocessor
 from data_preprocessing.movtit_nlp_v3 import MovieTitlesProcessor 
 
-#from data_preprocessing.data_preprocess_v5 import filter_data
-#from data_preprocessing.data_preprocess_v5 import create_rating_user_movie_info
-#from data_preprocessing.data_preprocess_v5 import perform_nlp_tasks
-#from data_preprocessing.data_preprocess_v5 import get_node_embeddings
+
 
