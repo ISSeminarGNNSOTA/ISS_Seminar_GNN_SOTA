c1f4dca4ad970826640f81cef94b3f189d7095ec
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-18 22:35:03 +0100

    Update __init__.py

--- hyperparameter_optimization/__init__ (3).py
+++ hyperparameter_optimization/__init__ (3).py
@@ -1,7 +1,5 @@
 # Ali Jaabous
 # __init__- file for the models 
 
-from models.graph_convolutional_network import GCN
-from models.graph_sage_network import GraphSAGE
-from models.graph_attentional_network import GAT
-from models.graph_predictor import Predictor
+from hyperparameter_optimization.graphStudy import GCN
+
