3c179fe7abe2a833ebab4f88ab8df21295ab0d24
ISSeminarGNNSOTA <162732442+ISSeminarGNNSOTA@users.noreply.github.com>
2024-03-18 22:45:49 +0100

    Update __init__.py

--- data_preprocessing/__init__.py
+++ data_preprocessing/__init__.py
@@ -1,5 +1,6 @@
 from data_preprocessing.movtit_nlp_v3 import MovieTitlesProcessor
 from data_preprocessing.data_preprocess_v5 import DataPreprocessor
+from data_preprocessing.graphDataBuilder2Fold import GraphDataBuilder2Fold
  
 
 
